9f4f86197ced7234e8755eb19c076b5652aa3580
Vincenzo Tito <vincenzotito@icloud.com>
2024-01-20 10:07:30 +0100

    correzione dopo aver visto la lezione

--- php_03_Tito.php
+++ php_03_Tito.php
@@ -1,68 +1,70 @@
 <?php
-class Company {
+class Company
+{
   public $name;
   public $location;
-  public $tot_employees = 0;
-  public static $avg_wage = 1500;
-  public static $annualExpense;
-  public static $totExpense;
+  public $tot_employees;
 
-  public function __construct($name, $location, $tot_employees){
+  static public $avg_wage = 1500;
+  static public $tot;
+
+  public function __construct($name, $location, $tot_employees = 0)
+  {
     $this->name = $name;
     $this->location = $location;
     $this->tot_employees = $tot_employees;
   }
-// metodo per stampare in console tutti i dipendenti di ogni azienda istanziata
-  public function showData(){
-    if($this->tot_employees > 0){
-    echo " L'ufficio $this->name con sede in $this->location ha ben $this->tot_employees dipendenti"."\n";
-    }else{
-      echo " L'ufficio $this->name con sede in $this->location non ha dipendenti"."\n";
+
+  // metodo per stampare in console tutti i dipendenti di ogni azienda istanziata
+  public function showData()
+  {
+    if ($this->tot_employees > 0) {
+      echo "L'ufficio $this->name con sede in $this->location ha ben $this->tot_employees dipendenti" . "\n";
+    } else {
+      echo "L'ufficio $this->name con sede in $this->location non ha dipendenti" . "\n";
     }
   }
 
   // metodo per calcolare la spesa annuale di ogni azienda
-  public function expense(){
-    self::$annualExpense = self::$avg_wage * $this->tot_employees * 12;
-    echo "La spesa annuale dell'azienda $this->name è uguale a " . self::$annualExpense."\n";
-    $this->totCompanyExpense(self::$annualExpense);
+  public function expence()
+  {
+    return self::$avg_wage * $this->tot_employees * 12 . "\n";
   }
-// metodo per tenere aggiornata la somma totale delle spese
-  public function totCompanyExpense($annualExpense){
-    self::$totExpense += self::$annualExpense;
+
+  // metodo per calcolare la spesa di ogni azienda mese per mese
+  public function monthExpence($month){
+    return $this->tot_employees * self::$avg_wage * $month;
   }
-// metodo statico per calcolare il totale delle spese di tutte le aziende
-  public static function totExpense(){
-    echo "La somma totale annuale delle aziende è" ." ". self::$totExpense;
-    self::$totExpense = 0;
+
+// metodo per calcolare la spesa totale di tutte le aziende
+  public function tot(){
+    self::$tot += $this->expence();
   }
+
 }
 
-// istanziamento delle 5 aziende
 
-$company = new Company("Aulab","Bari",50);
-$company2 = new Company("Bending Spoons","Milano",80);
-$company3 = new Company("Tsunami Nutrition","Roma",50);
-$company4 = new Company("Euro Top","Milano",0);
-$company5 = new Company("Winelivery","Milano",30);
+// istanziamento delle 5 aziende
+$company = new Company("Aulab", "Bari", 50);
+$company2 = new Company("Bending Spoons", "Milano", 80);
+$company3 = new Company("Tsunami Nutrition", "Roma", 50);
+$company4 = new Company("Euro Top", "Milano");
+$company5 =  new Company("Winelivery", "Milano", 30);
+
+
 
 var_dump($company);
-var_dump($company2);
-var_dump($company3);
-var_dump($company4);
-var_dump($company5);
 
 $company->showData();
-$company2->showData();
-$company3->showData();
-$company4->showData();
-$company5->showData();
 
-$company->expense();
-$company2->expense();
-$company3->expense();
-$company4->expense();
-$company5->expense();
+echo $company->expence();
 
-Company::totExpense();
+echo $company->monthExpence((int) readline("Inserisci il numero di mesi:  "))."\n";
 
+$company->tot();
+$company2->tot();
+$company3->tot();
+$company4->tot();
+$company5->tot();
+
+echo Company::$tot;
